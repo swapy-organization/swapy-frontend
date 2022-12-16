fac869c6886b54a5507ba6daa4ba0b1087c2aacf
Zaid alshibi <zaidealshibi@gmail.com>
2022-12-16 18:18:25 +0300

    created /swap page and added the carousel to it and added the buyOrSwap

---
+++ src/components/BuyOrSwap/Carousel.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Carousel} from "react-bootstrap";
+
+
+function ImgCarousel (props) {
+
+    return (
+        <div>
+            <Carousel fade>
+            {props.imgs && props.imgs.map( ( img, index ) => (
+                <Carousel.Item key={index}>
+                    <img
+                        src={img}
+                        className={'d-block w-100'}
+                        alt={'First slide'}
+                        style={{
+                            height: '500px' ,
+                            width: '100%'
+                            }}
+                    />
+                </Carousel.Item>
+            ))}
+            </Carousel>
+
+        </div>
+    );
+};
+
+export default ImgCarousel;
